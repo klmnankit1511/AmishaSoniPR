a6203b38979e1d098796064bbafde1e4d85cd8ec
Ankit <klmnankit1511@gmaill.com>
2023-02-04 22:35:31 +0530

    First Commit

---
+++ src/Component/Number.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Number(){
+    return(
+        <div>
+            <input type="text" placeholder="Amisha Soni"></input>
+            <input type="text" placeholder="Amisha Soni1"></input>
+            <input type="text" placeholder="Amisha Soni2"></input>
+            <input type="text" placeholder="Amisha Soni3"></input>
+            <input type="text" placeholder="Amisha Soni4"></input>
+        </div>
+    )
+}
